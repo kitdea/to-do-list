7e65e713411b57cd4f3fb10c9d87a92536322663
kitdea <francismarrosales@gmail.com>
2021-01-09 10:42:18 +0800

    starting new project

---
+++ app.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+loadEventListeners();
+
+function loadEventListeners() {
+  form.addEventListener('submit', addTask);
+}
+
+function addTask(e) {
+  if(taskInput.value === '') {
+    alert('Add task');
+  }
+  e.preventDefault();
+}
