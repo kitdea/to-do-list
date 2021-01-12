a5a5242adb1b4cf8b7a9b41dd6877560321abec5
kitdea <francismarrosales@gmail.com>
2021-01-13 06:28:37 +0800

    adding task

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const taskInput = document.querySelector('#task');
 
 loadEventListeners();
 
+// load all event listeners
 function loadEventListeners() {
+// add task
   form.addEventListener('submit', addTask);
 }
 
@@ -14,5 +16,20 @@ function addTask(e) {
   if(taskInput.value === '') {
     alert('Add task');
   }
+
+  const li = document.createElement('li');
+  li.className = 'collection-item';
+
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  li.appendChild(link);
+  taskList.appendChild(li);
+
+  taskInput.value = '';
+
+  console.log();
   e.preventDefault();
 }
